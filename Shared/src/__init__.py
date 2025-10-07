8fa2136ced173df8691f0d3cfe1f4b9bdd3671ae
Akash <104140404+akashsp7@users.noreply.github.com>
2025-10-07 17:10:12 -0400

    req

--- Shared/src/__init__.py
+++
@@ -1,3 +0,0 @@
-"""AgentX - DeepAgents Financial Research Platform"""
-
-__version__ = "0.1.0"
--- Shared/src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility modules for AgentX platform."""
-
-from .mcp_client import create_mcp_tools
-
-__all__ = ["create_mcp_tools"]
